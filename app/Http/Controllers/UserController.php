c5ccdac1ce0f708fcb106b6ac571ff3421fd32ad
pacman-ctm <phamanhcuongvna2511@gmail.com>
2021-11-05 21:59:31 +0700

    Add get_info, update_info API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id=null)
+    {
+        // xử lý nếu không có, nếu null
+        return User::find($id);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        // return $request->all();
+        // error_log($request->input("facebook_link"));
+        $user = $request->user;
+        // $user = User::findOrFail($id);
+        $user->update($request->all());
+        return response()->json(["success" => true]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Search user by email.
+     *
+     * @param  int  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name) {
+        return User::where("name_in_forum", "like", '%'.$name.'%')->get();
+    }
+}
