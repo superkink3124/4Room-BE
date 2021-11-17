49771969e13523c674b2e17d5dbc658d99be3f22
BuiChiTrung <trungcspntl@gmail.com>
2021-11-17 23:17:52 +0700

    modify api name, dat ten can than ho t cai

--- routes/api/notification.php
+++ routes/api/notification.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
 
-Route::get('/notification', [NotificationController::class, "notification"])->middleware("jwt.verify");
+Route::get('/notifications', [NotificationController::class, "notification"])->middleware("jwt.verify");
 Route::get('/noti/{id}', [NotificationController::class, "show"])->middleware('jwt.verify');
-Route::get('/count_unseen_notification', [NotificationController::class, "count_unseen_notification"])->middleware('jwt.verify');
-Route::post('/update_last_update_notification', [NotificationController::class, "update_last_update_notification"])->middleware('jwt.verify');
+Route::get('/notifications/count-unread', [NotificationController::class, "count_unseen_notification"])->middleware('jwt.verify');
+Route::post('/notifications/mark-as-read', [NotificationController::class, "update_last_update_notification"])->middleware('jwt.verify');
