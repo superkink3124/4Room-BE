f01f24a247bede1cada205f1c87d3434ecd8a835
superkink3124 <ducnhimngn@gmail.com>
2021-12-12 22:17:19 +0700

    Add trigger update post update at when comment

---
+++ database/migrations/2021_12_12_151322_create_trigger_update_post_update_at_when_comment.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTriggerUpdatePostUpdateAtWhenComment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("
+        CREATE TRIGGER `update_post_update_at` AFTER INSERT ON `comments` FOR EACH ROW BEGIN
+       
+       UPDATE posts
+       SET posts.updated_at = NOW()
+       WHERE posts.id = NEW.post_id;
+       
+       END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trigger_update_post_update_at_when_comment');
+    }
+}
