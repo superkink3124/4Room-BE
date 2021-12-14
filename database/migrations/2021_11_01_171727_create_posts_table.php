22170e99b5a1fba8b25fb67687981d5dd33baf64
Anh Duc <anhducpn67@gmail.com>
2021-12-14 09:50:28 +0700

    Add markdown in post

--- database/migrations/2021_11_01_171727_create_posts_table.php
+++ database/migrations/2021_11_01_171727_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("user_id");
+            $table->text("title");
             $table->text("content");
             $table->timestamps();
             $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade")->onUpdate("cascade");
